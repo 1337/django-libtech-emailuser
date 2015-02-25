e63b0a83f89589315c3ec5de67c8f73d5dd00c80
Darren Maki <darren@rideco.com>
2015-02-25 15:33:30 -0500

    Adding missing 'fields' to form.

--- emailuser/forms.py
+++ emailuser/forms.py
@@ -52,6 +52,7 @@ class EmailUserChangeForm(forms.ModelForm):
 
     class Meta:
         model = EmailUser
+        fields = ("email",)
 
     def __init__(self, *args, **kwargs):
         super(EmailUserChangeForm, self).__init__(*args, **kwargs)
