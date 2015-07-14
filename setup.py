c87530be902a33e95d3be73bd4cbdfec4d57e364
Darren Maki <darren@rideco.com>
2015-07-13 23:33:37 -0400

    Fixing paths for migrations.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-libtech-emailuser',
     version='0.2',
-    packages=['emailuser', 'migrations'],
+    packages=['emailuser', 'emailuser.migrations'],
     include_package_data=True,
     license='BSD License',
     description='Use emailaddress as username in Django +1.5',
