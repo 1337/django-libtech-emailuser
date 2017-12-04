6ffbd0f5de9cc0a1d075e746c45e39cd3d58d6c5
Brian Lai <brian@rideco.com>
2017-12-04 19:45:57 +0000

    Migrate this first
    Fixes XX-XXXX
    Required by XX-XXXX
    
    Modify the migration graph to run emailuser
    migrations before Django 1.10 auth migrations.
    Required for upgrade.
    
    Recommend bumping the version number.

--- emailuser/admin.py
+++ emailuser/admin.py
@@ -1,9 +1,16 @@
-from django import forms
+# coding=utf-8
+
+
+"""Modified from https://github.com/RideCo/django-libtech-emailuser."""
+
+
+# pylint: disable=invalid-name, protected-access
 from django.conf import settings
 from django.contrib import admin
 from django.contrib import messages
 from django.contrib.admin.utils import flatten_fieldsets
 from django.contrib.auth.forms import AdminPasswordChangeForm
+from django.core.exceptions import PermissionDenied
 from django.db import transaction
 from django.http import Http404
 from django.http import HttpResponseRedirect
@@ -15,28 +22,36 @@ from django.utils.translation import ugettext
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
-from emailuser.forms import EmailUserChangeForm
-from emailuser.forms import EmailUserCreationForm
+
+from emailuser.forms import EmailUserChangeForm, EmailUserCreationForm
 from emailuser.models import EmailUser
+
 
 csrf_protect_m = method_decorator(csrf_protect)
 sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
 
+
 class EmailUserAdmin(admin.ModelAdmin):
+    """Admin form for users. A subclass is actually used."""
+    # pylint: disable=redefined-builtin
     add_form_template = 'admin/auth/user/add_form.html'
     change_user_password_template = None
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (None, {'fields': ('email',
+                           'password')}),
+        (_('Personal info'), {'fields': ('first_name',
+                                         'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff',
+                                       'is_superuser',
+                                       'groups',
+                                       'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login',
+                                           'date_joined')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')}
-         ),
+            'fields': ('email', 'password1', 'password2')}),
     )
     form = EmailUserChangeForm
     add_form = EmailUserCreationForm
@@ -49,14 +64,13 @@ class EmailUserAdmin(admin.ModelAdmin):
     filter_horizontal = ('groups', 'user_permissions',)
 
     def get_fieldsets(self, request, obj=None):
+        """Unsure of usage."""
         if not obj:
             return self.add_fieldsets
         return super(EmailUserAdmin, self).get_fieldsets(request, obj)
 
     def get_form(self, request, obj=None, **kwargs):
-        """
-        Use special form during user creation
-        """
+        """Use special form during user creation."""
         defaults = {}
         if obj is None:
             defaults.update({
@@ -67,22 +81,28 @@ class EmailUserAdmin(admin.ModelAdmin):
         return super(EmailUserAdmin, self).get_form(request, obj, **defaults)
 
     def get_urls(self):
+        """Believed to retrieve some kind of URL.
+
+        Avoid using this method.
+        """
         from django.conf.urls import url
         return [
             url(r'^(\d+)/password/$',
-             self.admin_site.admin_view(self.user_change_password))
+                self.admin_site.admin_view(self.user_change_password))
         ] + super(EmailUserAdmin, self).get_urls()
 
     @sensitive_post_parameters_m
     @csrf_protect_m
     @transaction.atomic
     def add_view(self, request, form_url='', extra_context=None):
-        # It's an error for a user to have add permission but NOT change
-        # permission for users. If we allowed such users to add users, they
-        # could create superusers, which would mean they would essentially have
-        # the permission to change users. To avoid the problem entirely, we
-        # disallow users from adding users if they don't have change
-        # permission.
+        """It is an error for a user to have add permission but NOT
+        change permission for users. If we allowed such users to add
+        users, they could create superusers, which would mean they would
+        essentially have the permission to change users.
+
+        To avoid the problem entirely, we disallow users from adding
+        users if they do not have change permission.
+        """
         if not self.has_change_permission(request):
             if self.has_add_permission(request) and settings.DEBUG:
                 # Raise Http404 in debug mode so that the user gets a helpful
@@ -92,7 +112,7 @@ class EmailUserAdmin(admin.ModelAdmin):
                     'order to add users, Django requires that your user '
                     'account have both the "Add user" and "Change user" '
                     'permissions set.')
-            raise PermissionDenied
+            raise PermissionDenied()
         if extra_context is None:
             extra_context = {}
         username_field = self.model._meta.get_field(self.model.USERNAME_FIELD)
@@ -106,8 +126,9 @@ class EmailUserAdmin(admin.ModelAdmin):
 
     @sensitive_post_parameters_m
     def user_change_password(self, request, id, form_url=''):
+        """Believed to allow a user to change the password."""
         if not self.has_change_permission(request):
-            raise PermissionDenied
+            raise PermissionDenied()
         user = get_object_or_404(self.get_queryset(request), pk=id)
         if request.method == 'POST':
             form = self.change_password_form(user, request.POST)
@@ -120,11 +141,11 @@ class EmailUserAdmin(admin.ModelAdmin):
             form = self.change_password_form(user)
 
         fieldsets = [(None, {'fields': list(form.base_fields)})]
-        adminForm = admin.helpers.AdminForm(form, fieldsets, {})
+        admin_form = admin.helpers.AdminForm(form, fieldsets, {})
 
         context = {
             'title': _('Change password: %s') % escape(user.get_username()),
-            'adminForm': adminForm,
+            'adminForm': admin_form,
             'form_url': form_url,
             'form': form,
             'is_popup': '_popup' in request.GET or '_popup' in request.POST,
@@ -141,7 +162,7 @@ class EmailUserAdmin(admin.ModelAdmin):
         return TemplateResponse(request,
                                 self.change_user_password_template or
                                 'admin/auth/user/change_password.html',
-                                context, current_app=self.admin_site.name)
+                                context)
 
     def response_add(self, request, obj, post_url_continue=None):
         """
@@ -158,34 +179,6 @@ class EmailUserAdmin(admin.ModelAdmin):
             request.POST['_continue'] = 1
         return super(EmailUserAdmin, self).response_add(request, obj,
                                                         post_url_continue)
-
-
-class EmailUserCreationForm(forms.ModelForm):
-    """A form for creating new users. Includes all the required
-    fields, plus a repeated password."""
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        label='Password confirmation', widget=forms.PasswordInput)
-
-    class Meta:
-        model = EmailUser
-        fields = ('email', 'first_name', 'last_name')
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
 
 
 admin.site.register(EmailUser, EmailUserAdmin)
--- emailuser/forms.py
+++ emailuser/forms.py
@@ -1,14 +1,20 @@
+# coding=utf-8
+
+
+"""Modified from https://github.com/RideCo/django-libtech-emailuser."""
+
 from __future__ import unicode_literals
+
 from django import forms
-from django.utils.translation import ugettext_lazy as _
-from emailuser.models import EmailUser
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import ugettext_lazy as _
+
+from emailuser.models import EmailUser
 
 
 class EmailUserCreationForm(forms.ModelForm):
-    """
-    A form that creates a user, with no privileges, from the given email and
-    password.
+    """A form that creates a user, with no privileges, from the given
+    email and password.
     """
     error_messages = {
         'duplicate_email': _("A user with that  email already exists."),
@@ -16,17 +22,22 @@ class EmailUserCreationForm(forms.ModelForm):
     }
     email = forms.EmailField(label=_("Email"))
 
-    password1 = forms.CharField(label=_("Password"),
-                                widget=forms.PasswordInput)
-    password2 = forms.CharField(label=_("Password confirmation"),
-                                widget=forms.PasswordInput,
-                                help_text=_("Enter the same password as above, for verification."))
+    password1 = forms.CharField(
+        label=_("Password"),
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        help_text=_(
+            "Enter the same password as above, for verification."))
 
-    class Meta:
+    class Meta(object):
+        """Inner Meta class."""
         model = EmailUser
         fields = ("email",)
 
     def clean_password2(self):
+        """Unsure of usage."""
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
@@ -35,6 +46,7 @@ class EmailUserCreationForm(forms.ModelForm):
         return password2
 
     def save(self, commit=True):
+        """Sets the password if one is provided."""
         user = super(EmailUserCreationForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         if commit:
@@ -43,49 +55,72 @@ class EmailUserCreationForm(forms.ModelForm):
 
 
 class EmailUserChangeForm(forms.ModelForm):
+    """Presumably allows a user's password to be changed.
+
+    Unsure of actual usage.
+    """
     email = forms.EmailField(label=_("Email"))
 
-    password = ReadOnlyPasswordHashField(label=_("Password"),
-                                         help_text=_("Raw passwords are not stored, so there is no way to see "
-                                                     "this user's password, but you can change the password "
-                                                     "using <a href=\"password/\">this form</a>."))
+    password = ReadOnlyPasswordHashField(
+        label=_("Password"),
+        help_text=_(
+            "Raw passwords are not stored, so there is no way to see "
+            "this user's password, but you can change the password "
+            "using <a href=\"password/\">this form</a>."))
 
-    class Meta:
+    class Meta(object):
+        """Inner Meta class."""
         model = EmailUser
         fields = ("email",)
 
     def __init__(self, *args, **kwargs):
         super(EmailUserChangeForm, self).__init__(*args, **kwargs)
-        f = self.fields.get('user_permissions', None)
-        if f is not None:
-            f.queryset = f.queryset.select_related('content_type')
+        perms = self.fields.get('user_permissions', None)
+        if perms is not None:
+            perms.queryset = perms.queryset.select_related(
+                'content_type')
 
     def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
+        """
+        Regardless of what the user provides, return the initial value.
+
+        This is done here, rather than on the field, because the
+        field does not have access to the initial value.
+        """
         return self.initial["password"]
 
+
 class PasswordResetRequestForm(forms.Form):
+    """Presumably allows a user's password to be reset.
+
+    Unsure of actual usage.
+    """
     email = forms.CharField(label=_("Email"))
 
     def clean_email(self):
+        """Unsure of usage."""
         email = self.cleaned_data.get("email")
         if not EmailUser.objects.filter(email=email).exists():
-            raise forms.ValidationError(_("No user with that email exists"))
+            raise forms.ValidationError(
+                _("No user with that email exists"))
         return email
 
-class PasswordResetForm(forms.Form):
 
+class PasswordResetForm(forms.Form):
+    """Presumably allows a user's password to be reset.
+
+    Unsure of actual usage.
+    """
     password1 = forms.CharField(label=_("Password"),
                                 widget=forms.PasswordInput)
     password2 = forms.CharField(label=_("Password confirmation"),
                                 widget=forms.PasswordInput)
 
-
     def clean_password2(self):
+        """Unsure of usage."""
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError(_("The two passwords did not match"))
+            raise forms.ValidationError(
+                _("The two passwords did not match"))
         return password2
